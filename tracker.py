f8b4cf0b9bdb7aba1535ff3f21d549f5afbd12f9
Orest Tokovenko <oresttokovenko@gmail.com>
2021-03-12 09:43:40 -0700

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-# create a function to get price of cryptocurrency
+# create a function to obtain price of cryptocurrency
 
 
 def get_latest_crypto_price(coin):
     url = 'https://www.google.com/search?q=' + (coin) + 'price'
     # make a request to the website
     HTML = requests.get(url)
-    # Parsse the HTML
+    # parse the HTML
     soup = BeautifulSoup(HTML.text, 'html.parser')
     # find the current price
     texti = soup.find('div', attrs={
